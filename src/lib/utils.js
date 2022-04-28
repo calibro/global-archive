6174d0699c531afd3a985a39c909c2f6b1250b3d
uf0 <scnepa222@gmail.com>
2022-04-28 14:39:26 +0200

    close #1

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,23 +1,25 @@
 import Vibrant from 'node-vibrant/lib/browser.js';
 
 export const ATTRIBS = [
-	{ key: 'Keywords', multiple: true, group: true, table_name: 'Keywords' },
-	{ key: 'Main language of collection', multiple: true, group: true, table_name: 'Language' },
+	{ key: 'Start year', multiple: false, group: false },
+	{ key: 'End year', multiple: false, group: false },
 	{ key: 'Region concerned', multiple: true, group: true, table_name: 'Region concerned' },
 	{ key: 'State/Nation', multiple: true, group: true, table_name: 'State/Nation' },
-	{ key: 'Type of sources', multiple: true, group: true, table_name: 'Type of sources' },
+	{ key: 'Organisation', multiple: true, group: true, table_name: 'Organisation' },
 	{ key: 'Host institution', multiple: true, group: true, table_name: 'Host institution' },
-	{ key: 'Brief description of collection', multiple: false, group: false },
-	{ key: 'Start year', multiple: false, group: false },
-	{ key: 'End year', multiple: false, group: false }
+	{ key: 'Keywords', multiple: true, group: true, table_name: 'Keywords' },
+	{ key: 'Type of sources', multiple: true, group: true, table_name: 'Type of sources' },
+	{ key: 'Main language of collection', multiple: true, group: true, table_name: 'Language' },
+	{ key: 'Brief description of collection', multiple: false, group: false }
 ];
 
 export const METADATA = [
+	'State/Nation',
+	'Organisation',
 	'Host institution',
 	'Keywords',
-	'Main language of collection',
 	'Type of sources',
-	'State/Nation'
+	'Main language of collection'
 ];
 
 export const HEADERS = [
