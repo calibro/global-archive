618d1f9ed14563699736d8679c4b8ec1bf4e0986
uf0 <scnepa222@gmail.com>
2022-04-29 17:29:50 +0200

    forgot component

---
+++ src/lib/Search.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { slide } from 'svelte/transition';
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	let searchText;
+	function filterDimension() {
+		filterCf(searchText);
+	}
+
+	function filterCf(value) {
+		dispatch('filterCf', {
+			value: value
+		});
+	}
+</script>
+
+<div class="w-100">
+	<input
+		class="form-control rounded-pill border-dark"
+		type="text"
+		placeholder="Type to search..."
+		bind:value={searchText}
+		on:input={(e) => filterDimension()}
+	/>
+</div>
