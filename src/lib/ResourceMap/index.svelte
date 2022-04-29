6d514f7a1ae2f6a10aaef3326e075580e413d36e
uf0 <scnepa222@gmail.com>
2022-04-29 14:09:54 +0200

    close #3

--- src/lib/ResourceMap/index.svelte
+++ src/lib/ResourceMap/index.svelte
@@ -1,8 +1,8 @@
 <script>
 	import { getContext } from 'svelte';
 	import { feature } from 'topojson-client';
-	import { geoPath } from 'd3-geo';
-	import { geoPeirceQuincuncial } from 'd3-geo-projection';
+	import { geoPath, geoGraticule10 } from 'd3-geo';
+	import { geoPeirceQuincuncial, geoWinkel3 } from 'd3-geo-projection';
 	import { csvParse } from 'd3-dsv';
 	import countries from './countries.json';
 	import oceans from './oceans.csv?raw';
@@ -11,42 +11,46 @@
 	const { width, height } = getContext('responsive');
 	$: mHeight = $width < 500 ? $width : 500;
 
-	$: projection = geoPeirceQuincuncial()
-		.fitSize([$width, mHeight], { type: 'Sphere' })
-		.precision(0.1);
+	$: projection = geoWinkel3().fitSize([$width, mHeight], { type: 'Sphere' }); //.precision(0.1);
 
 	$: path = geoPath().projection(projection);
 
 	const topoData = feature(countries, 'collection');
-	const land = {
-		...topoData,
-		features: topoData.features.filter((d) => d.properties.ISO_A3 !== 'ATA')
-	};
-
-	const data = land.features;
-
+	const data = topoData.features;
 	const oceansData = csvParse(oceans);
-	console.log(oceansData, regions_concerned);
+	const outline = { type: 'Sphere' };
+	const graticule = geoGraticule10();
 </script>
 
 <svg width={$width} height={mHeight}>
-	{#each data as feature}
-		<path
-			d={path(feature)}
-			class="borderc"
-			fill={regions_concerned.includes(feature.properties.regions_concerned) ? '#FFE000' : 'black'}
-		/>
-	{/each}
-	{#each oceansData as ocean}
-		{#if regions_concerned.includes(ocean.name)}
-			<text
-				class="label"
-				text-anchor="middle"
-				transform="translate({projection([+ocean.longitude, +ocean.latitude]) + ''})"
-				dy="0.35em">{ocean.name}</text
-			>
-		{/if}
-	{/each}
+	<defs>
+		<path id="outline" d={path(outline)} />
+		<clipPath id="clip"><use xlink:href="#outline" /></clipPath>
+	</defs>
+	<g clip-path="url(#clip)">
+		<use xlink:href="#outline" fill="#fff" />
+		<path d={path(graticule)} fill="none" stroke="#ccc" />
+		{#each data as feature}
+			<path
+				d={path(feature)}
+				class="borderc"
+				fill={regions_concerned.includes(feature.properties.regions_concerned)
+					? '#FFE000'
+					: 'black'}
+			/>
+		{/each}
+		{#each oceansData as ocean}
+			{#if regions_concerned.includes(ocean.name)}
+				<text
+					class="label"
+					text-anchor="middle"
+					transform="translate({projection([+ocean.longitude, +ocean.latitude]) + ''})"
+					dy="0.35em">{ocean.name}</text
+				>
+			{/if}
+		{/each}
+	</g>
+	<use xlink:href="#outline" fill="none" stroke="#000" />
 </svg>
 
 <style>
